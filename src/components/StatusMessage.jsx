338819c39e22548e34a467745e5e721ea8538916
Hp <Hp@DESKTOP-8HIKA16.(none)>
2023-06-25 16:35:14 +0530

    added game history

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,34 +1,36 @@
-const statusMessage = ({ winner, isXNext, squares }) => {
+const statusMessage = ({ winner, gamingBoard }) => {
+  const { squares, isXNext } = gamingBoard;
+
   const noMovesLeft = squares.every(squareValue => squareValue !== null);
   const nextPlayer = isXNext ? 'X' : 'O';
 
   const renderStatusMessage = () => {
     if (winner) {
       return (
-        <div>
+        <>
           Winner is{' '}
           <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
             {winner}
           </span>
-        </div>
+        </>
       );
     }
     if (!winner && noMovesLeft) {
       return (
-        <div>
+        <>
           <span className="text-orange">O</span> and{' '}
           <span className="text-green">X</span> tied
-        </div>
+        </>
       );
     }
     if (!winner && !noMovesLeft) {
       return (
-        <div>
+        <>
           Next Player is{' '}
           <span className={isXNext ? 'text-green' : 'text-orange'}>
             {nextPlayer}
           </span>
-        </div>
+        </>
       );
     }
     return null;
